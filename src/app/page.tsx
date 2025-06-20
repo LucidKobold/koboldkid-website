9cb090fd16c6a3df587f3ebfdc5dfe6e5aa86b1c
Lucid Kobold <72232219+LucidKobold@users.noreply.github.com>
2025-06-20 19:28:46 -0400

    Added Nextjs and ChalkraUI boilerplate.

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function Home() {
+  return <Box h="100vh" w="100vw" display="flex" justifyContent="center" alignItems="center"><Heading color="cyan" fontSize="4rem">{"Hello Kiddo!"}</Heading></Box>;
+}
